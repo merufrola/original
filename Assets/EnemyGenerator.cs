0eaec5914bcb2fb1d0eacd8f4f22be1b3adcd168
yuuka_hirosugi <merufrola@gmail.com>
2016-06-22 12:33:59 +0900

    ジェネレーターと敵作った。

---
+++ Assets/EnemyGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyGenerator : MonoBehaviour {
+    public GameObject enemy;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Generate", 0, 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void Generte (){
+        Instantiate(enemy, transform.position, transform.rotation);
+    }
+}
