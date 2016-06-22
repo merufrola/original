f2b5fe06e8b3ec7c2e1e73e314ab294a66b895f3
yuuka_hirosugi <merufrola@gmail.com>
2016-06-22 21:01:02 +0900

    バグ（魔の領域）

---
+++ Assets/Unitychan.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unitychan : MonoBehaviour {
+    Animator animator;
+    public float speed = 5.0f;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+            float pos_x = transform.position.x;
+        }
+        Debug.Log(transform.position);
+
+
+
+        if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+            animator.SetBool("TurnL", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            animator.SetBool("TurnL", false);
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetBool("RUn", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            animator.SetBool("RUn", false);
+        }
+
+        TRP();
+	}
+    void TRP()
+    {
+        int distance = 10;
+        
+    }
+}
